c70184110fc191733a07d1ab04f4ab217fe0a452
bjpogs <bjpogs26@gmail.com>
2022-06-14 12:04:28 +0800

    update

--- src/components/templates/Navbar.js
+++ src/components/templates/Navbar.js
@@ -28,6 +28,7 @@ const Navbars = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <div class="ms-auto"/>
                     <Nav>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/shop">Shop</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
                         <Nav.Link href="/reservation">Reservation</Nav.Link>
